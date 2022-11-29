0ea8fcdfe58f498ed693176bbd496c9869ad1ea0
Christian Manzaraz <magnusmanz@gmail.com>
2022-11-29 00:26:27 -0300

    Create Programatic Segues

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -8,18 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var textField: UITextField!
+    @IBOutlet var segueSwitch: UISwitch!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {   }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = textField.text
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
     }
-
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+            
+        }
+    }
+    
 }
 
